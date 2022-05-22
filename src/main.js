c85aae5830fe5f2d9c3739bee2e0a90186944909
DevHumbleChris <dev.humblechris254@gmail.com>
2022-05-22 07:35:18 +0300

    Saving commit changes

--- src/main.js
+++ src/main.js
@@ -3,5 +3,12 @@ import App from './App.vue'
 import { createPinia } from 'pinia'
 import './index.css'
 import router from './routes'
+// import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+// import { library } from "@fortawesome/fontawesome-svg-core";
+// import { fas } from "@fortawesome/free-solid-svg-icons";
+// import { fab } from "@fortawesome/free-solid-svg-icons";
+// import { far } from "@fortawesome/free-solid-svg-icons";
+
+// library.add(fas, fab, far)
 
 createApp(App).use(createPinia()).use(router).mount('#app')
