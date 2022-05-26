315f5aedf96c616fc5f61d7fc21af9cfb00b5275
DevHumbleChris <devhumblechris254@gmail.com>
2022-05-26 12:32:52 +0300

    Added fontawesome icons

--- src/main.js
+++ src/main.js
@@ -6,18 +6,19 @@ import router from './routes'
 import Toast from 'vue-toastification'
 import 'vue-toastification/dist/index.css'
 import BaseCard from './components/BaseCard.vue'
-// import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-// import { library } from "@fortawesome/fontawesome-svg-core";
-// import { fas } from "@fortawesome/free-solid-svg-icons";
-// import { fab } from "@fortawesome/free-solid-svg-icons";
-// import { far } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
 
-// library.add(fas, fab, far)
+library.add(fas, fab, far)
 
 const options = {}
 
 createApp(App)
     .component('BaseCard', BaseCard)
+    .component('FontAwesomeIcon', FontAwesomeIcon)
     .use(createPinia())
     .use(router)
     .use(Toast, options)
--- src/views/Error404/index.vue
+++ src/views/Error404/index.vue
@@ -1,19 +1,21 @@
 <template>
-  <div class="min-h-screen flex items-center">
+    <div class="min-h-screen flex items-center bg-404">
     <div class="container mx-auto p-4 flex flex-wrap items-center">
       <div class="w-full md:w-5/12 text-center p-4">
-        <img src="https://themichailov.com/img/not-found.svg" alt="Not Found" />
+        <img src="../../assets/error-img.png" alt="Error Logo" />
       </div>
       <div class="w-full md:w-7/12 text-center md:text-left p-4">
-        <div class="text-6xl font-medium">404</div>
         <div class="text-xl md:text-3xl font-medium mb-4">
           Oops. This page has gone missing.
         </div>
         <div class="text-lg mb-8">
           You may have mistyped the address or the page may have moved.
         </div>
-        <a href="#" class="border border-white bg-pink-300 text-white p-4 rounded-xl"
-          >Go Home</a
+        <router-link to="/" class="btn-404 text-white p-4 rounded-xl"
+          >
+          <FontAwesomeIcon :icon="['fas', 'home']" class="mr-1" />
+          Return Home
+          </router-link
         >
       </div>
     </div>
