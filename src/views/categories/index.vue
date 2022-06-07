96e47b7412b3838838824630285f2cc8069f5c8a
DevHumbleChris <devhumblechris254@gmail.com>
2022-06-07 17:23:09 +0300

    Updated Category Component

--- src/views/categories/index.vue
+++ src/views/categories/index.vue
@@ -2,15 +2,12 @@
 import { ref, onMounted } from "vue";
 import Modal from "../../components/Modal.vue";
 import axios from "axios";
+import { useToast } from "vue-toastification";
+
+const toast = useToast();
 
 const showModal = ref(false);
-
-const openModal = () => {
-  showModal.value = !showModal.value;
-  console.log(showModal.value);
-};
-
-const show = ref(null);
+const errorMessage = ref("");
 
 const categoryData = ref({
   name: "",
@@ -18,12 +15,8 @@ const categoryData = ref({
   color: "#003d5b",
 });
 
-const handleSubmit = () => {
-  console.log(categoryData.value);
-};
-
-const cancelSubmit = () => {
-  openModal();
+const openModal = () => {
+  showModal.value = !showModal.value;
   categoryData.value = {
     name: "",
     monthlyBudget: "",
@@ -31,14 +24,60 @@ const cancelSubmit = () => {
   };
 };
 
+const show = ref(null);
+
+const handleSubmit = () => {
+  axios
+    .post("/category/create", {
+      name: categoryData.value.name,
+      color: categoryData.value.color,
+      monthlyBudget: parseInt(categoryData.value.monthlyBudget),
+    })
+    .then((resp) => {
+      if (resp.data.category) {
+        openModal();
+        toast.success(`Category ${resp.data.category.name} Added Successfully`, {
+          timeout: 2500,
+        });
+      }
+      if (resp.data.message) {
+        toast.info(`${resp.data.message}`, {
+          timeout: 2500,
+        });
+      }
+    })
+    .catch((err) => {
+      errorMessage.value = err.message;
+      console.log(err);
+      toast.error(`${err.message}`, {
+        timeout: 2500,
+      });
+    });
+};
+
+const cancelSubmit = () => {
+  openModal();
+  categoryData.value = {
+    name: "",
+    monthlyBudget: 0,
+    color: "#003d5b",
+  };
+  toast.info("Operation Canceled Successfully", {
+    timeout: 2500,
+  });
+};
+
+const categories = ref([]);
+
 onMounted(() => {
   axios
     .get("/categories")
-    .then((data) => {
-      alert(data);
+    .then((resp) => {
+      categories.value = resp.data.categories;
+      console.log(resp.data.categories);
     })
     .catch((err) => {
-      alert(err.message);
+      errorMessage.value = err.message;
     });
 });
 </script>
@@ -128,7 +167,8 @@ onMounted(() => {
                       <div class="flex my-2">
                         <label
                           for="categoryColor"
-                          :class="`w-10 h-10 bg-[${categoryData.color}] rounded-xl mr-3`"
+                          class="w-10 h-10 rounded-xl mr-3"
+                          :style="`background-color:${categoryData.color};`"
                         ></label>
                         <input
                           v-model="categoryData.color"
@@ -203,85 +243,48 @@ onMounted(() => {
               <th class="font-normal text-left pl-4">Category</th>
               <th class="font-normal text-left pl-12">Monthly Budget</th>
               <th class="font-normal text-left pl-20">Color</th>
+              <th class="font-normal text-left pl-20"></th>
             </tr>
           </thead>
           <tbody class="w-full">
             <tr
+              v-for="category in categories"
+              :key="category._id"
               tabindex="0"
               class="focus:outline-none h-20 text-sm leading-none text-gray-800 bg-white hover:bg-gray-100 border-b border-t border-gray-100"
             >
               <td class="pl-4 cursor-pointer">
                 <div class="flex items-center">
-                  <div class="w-10 h-10 bg-pink-300 rounded-xl"></div>
+                  <div
+                    class="w-10 h-10 rounded-xl"
+                    :style="`background-color: ${category.color}`"
+                  ></div>
                   <div class="pl-4">
-                    <p class="font-medium">UX Design &amp; Visual Strategy</p>
+                    <p class="font-medium">{{ category.name }}</p>
                     <p class="text-xs leading-3 text-gray-600 pt-2">Cash</p>
                   </div>
                 </div>
               </td>
               <td class="pl-12">
-                <p class="font-medium">$13,000</p>
+                <p class="font-medium">{{ category.monthlyBudget }}</p>
                 <p class="text-xs leading-3 text-gray-600 mt-2">$4,200 left</p>
               </td>
               <td class="pl-20">
                 <div class="w-10 h-10 bg-pink-300 rounded-xl"></div>
               </td>
-              <td class="px-7 2xl:px-0">
+              <td class="px-2 xl:px-0">
                 <button
-                  @click="show === null ? (show = 0) : (show = null)"
-                  class="focus:ring-2 rounded-md focus:outline-none ml-7"
-                  role="button"
-                  aria-label="options"
+                  tabindex="0"
+                  class="focus:outline-none focus:text-indigo-600 text-xs w-full cursor-pointer"
                 >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="20"
-                    height="20"
-                    viewBox="0 0 20 20"
-                    fill="none"
-                  >
-                    <path
-                      d="M4.16667 10.8334C4.62691 10.8334 5 10.4603 5 10.0001C5 9.53984 4.62691 9.16675 4.16667 9.16675C3.70643 9.16675 3.33334 9.53984 3.33334 10.0001C3.33334 10.4603 3.70643 10.8334 4.16667 10.8334Z"
-                      stroke="#A1A1AA"
-                      stroke-width="1.25"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    ></path>
-                    <path
-                      d="M10 10.8334C10.4602 10.8334 10.8333 10.4603 10.8333 10.0001C10.8333 9.53984 10.4602 9.16675 10 9.16675C9.53976 9.16675 9.16666 9.53984 9.16666 10.0001C9.16666 10.4603 9.53976 10.8334 10 10.8334Z"
-                      stroke="#A1A1AA"
-                      stroke-width="1.25"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    ></path>
-                    <path
-                      d="M15.8333 10.8334C16.2936 10.8334 16.6667 10.4603 16.6667 10.0001C16.6667 9.53984 16.2936 9.16675 15.8333 9.16675C15.3731 9.16675 15 9.53984 15 10.0001C15 10.4603 15.3731 10.8334 15.8333 10.8334Z"
-                      stroke="#A1A1AA"
-                      stroke-width="1.25"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    ></path>
-                  </svg>
+                  <FontAwesomeIcon :icon="['far', 'edit']" class="mx-2" />
                 </button>
-                <div
-                  class="dropdown-content bg-white shadow w-24 absolute z-30 right-0 mr-6"
-                  v-if="show === 0"
+                <button
+                  tabindex="0"
+                  class="focus:text-indigo-600 text-xs w-full cursor-pointer"
                 >
-                  <div
-                    tabindex="0"
-                    class="focus:outline-none focus:text-indigo-600 text-xs w-full hover:bg-indigo-700 py-4 px-4 cursor-pointer hover:text-white"
-                  >
-                    <FontAwesomeIcon :icon="['far', 'edit']" class="mx-2" />
-                    <span>Edit</span>
-                  </div>
-                  <div
-                    tabindex="0"
-                    class="focus:outline-none focus:text-indigo-600 text-xs w-full hover:bg-red-700 py-4 px-4 cursor-pointer hover:text-white"
-                  >
-                    <FontAwesomeIcon :icon="['fas', 'trash']" class="mx-2" />
-                    <span>Delete</span>
-                  </div>
-                </div>
+                  <FontAwesomeIcon :icon="['fas', 'trash']" class="mx-2" />
+                </button>
               </td>
             </tr>
           </tbody>
